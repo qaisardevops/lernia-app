d51f4daf2d6e70f526255dd40066d4629038892b
Jonas Björk <jonas.bjork@gmail.com>
2025-01-02 11:36:20 +0100

    Changed to 0.0.0.0

--- src/main.go
+++ src/main.go
@@ -31,7 +31,7 @@ func main() {
     router.GET("/albums/:id", getAlbumByID)
     router.POST("/albums", postAlbums)
 
-    router.Run("localhost:8080")
+    router.Run("0.0.0.0:8080")
 }
 
 // getAlbums responds with the list of all albums as JSON.
